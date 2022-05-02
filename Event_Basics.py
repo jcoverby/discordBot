6a119754500b75ea86b9a6895b515b481c90c0ea
CarlosMInfante <83223613+CarlosMInfante@users.noreply.github.com>
2022-05-02 12:55:11 -0400

    Creating the bot
    Creating the admin bot and brining him online.

---
+++ Event_Basics.py
@@ -0,0 +1,8 @@
+import discord
+
+# This allows communication with the discord program
+bot = discord.Client()
+
+# This is the authentication token for my bot
+# Once run this will bring the bot online
+bot.run("OTcwNzI4MTEyOTkwMDg5MjQ2.YnALAQ.VX387HF7wbOb-BesnXWAsTDE318")
